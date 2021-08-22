2b2853b825f2ff87e1ccb7a60a026d4226a0269e
Gary Yang <garyyang0236@gmail.com>
2021-08-23 00:10:52 +0800

    refactor

--- SpecFlowDemo/SpecFlowDemo/TennisScoreHelper.cs
+++ SpecFlowDemo/SpecFlowDemo/TennisScoreHelper.cs
@@ -8,20 +8,22 @@ namespace SpecFlowDemo
     {
         public static string GetDisplayScore(int playerAScore, int playerBScore)
         {
-            var displayScoreMapper=new List<String>(){"Love","Fifteen","Thirty","Forty"};
+            var scoreMapper=new List<String>(){"Love","Fifteen","Thirty","Forty"};
+            var dominantPlayer = new List<string>(){ "Player A" };
+            var matchStatus = new List<string>() {"Adv", "Win"};
             if (playerAScore.Equals(playerBScore))
             {
-                return playerAScore>=3 ? "Deuce" : $"{displayScoreMapper[playerAScore]} All";
+                return playerAScore>=3 ? "Deuce" : $"{scoreMapper[playerAScore]} All";
             }
             if (playerAScore.Equals(4) && playerBScore.Equals(3))
             {
-                return "Player A Adv";
+                return $"{dominantPlayer[0]} {matchStatus[0]}";
             }
             if (playerAScore.Equals(5) && playerBScore.Equals(3))
             {
-                return "Player A Win";
+                return $"{dominantPlayer[0]} {matchStatus[1]}";
             }
-            return $"{displayScoreMapper[playerAScore]} {displayScoreMapper[playerBScore]}";
+            return $"{scoreMapper[playerAScore]} {scoreMapper[playerBScore]}";
             
         }
     }
