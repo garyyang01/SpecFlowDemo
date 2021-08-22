7b285438630a85ea237be72a01232539e7add246
Gary Yang <garyyang0236@gmail.com>
2021-08-23 00:12:09 +0800

    Pass 14 test case

--- SpecFlowDemo/SpecFlowDemo/TennisScoreHelper.cs
+++ SpecFlowDemo/SpecFlowDemo/TennisScoreHelper.cs
@@ -9,7 +9,7 @@ namespace SpecFlowDemo
         public static string GetDisplayScore(int playerAScore, int playerBScore)
         {
             var scoreMapper=new List<String>(){"Love","Fifteen","Thirty","Forty"};
-            var dominantPlayer = new List<string>(){ "Player A" };
+            var dominantPlayer = new List<string>(){ "Player A" ,"Player B"};
             var matchStatus = new List<string>() {"Adv", "Win"};
             if (playerAScore.Equals(playerBScore))
             {
@@ -22,6 +22,10 @@ namespace SpecFlowDemo
             if (playerAScore.Equals(5) && playerBScore.Equals(3))
             {
                 return $"{dominantPlayer[0]} {matchStatus[1]}";
+            }
+            if (playerAScore.Equals(3) && playerBScore.Equals(4))
+            {
+                return $"{dominantPlayer[1]} {matchStatus[0]}";
             }
             return $"{scoreMapper[playerAScore]} {scoreMapper[playerBScore]}";
             
